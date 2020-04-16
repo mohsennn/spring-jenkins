34699fcfce9bf6907efd0e33202fca764666d392
GHANEM <mohsenghanem02@gmail.com>
2020-04-16 02:03:36 +0200

    remove logger statement

--- src/test/java/com/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/jenkins/api/SpringJenkinsApplicationTests.java
@@ -18,7 +18,6 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing ...");
-		logger.info("Test case executing second log statament ...");
 		assertEquals(true, true);
 	}
 
